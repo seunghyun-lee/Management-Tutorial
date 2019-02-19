7cbc630e28ac84d3a0294d98155851e5bd92a62d
seunghyun-lee <seunghyunleekr@gmail.com>
2019-02-19 21:53:38 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Customer from './components/Customer';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Customer/>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Customer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Customer extends React.Component {
+    render() {
+        return (
+            <div>
+            <p2>홍길동</p2>
+            <p>aaaaaa</p>
+        </div>
+        )        
+    }
+}
+
+export default Customer;
